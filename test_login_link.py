d3b358faaa9dcc36580b10eb5ec0ac5bb0a52681
Ymoney Man <mxm.kitaev@gmail.com>
2020-06-06 21:07:42 +0300

    new initialization method

--- test_login_link.py
+++ test_login_link.py
@@ -1,8 +1,11 @@
 from pages.login_page import LoginPage
+from pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+link = "http://selenium1py.pythonanywhere.com"
 
-def test_login(browser):
-    page = LoginPage(browser,link)
+def test_guest_can_go_login_page(browser):
+    page = MainPage(browser,link)
     page.open()
-    page.should_be_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser,browser.current_url)
+    login_page.should_be_login_page()
